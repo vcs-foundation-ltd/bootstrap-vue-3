164dfa7585e84f3c831eb26734b18766ebe565a7
Carlos Bonadeo <carlosbonadeo@gmail.com>
2022-02-20 22:43:51 -0300

    Update BTooltip.vue

--- src/components/BTooltip/BTooltip.vue
+++ src/components/BTooltip/BTooltip.vue
@@ -13,14 +13,25 @@
 import {computed, onMounted, PropType, ref} from 'vue'
 import {createPopper, Placement} from '@popperjs/core'
 import type {ColorVariant} from 'src/types'
+import type { Tooltip } from "bootstrap";
 import useEventListener from '../../composables/useEventListener'
+
+const showEventDictionary = {
+  hover: "mouseenter",
+  focus: "focus",
+};
+
+const hideEventDictionary = {
+  hover: "mouseleave",
+  focus: "blur",
+};
 
 const props = defineProps({
   target: {type: String, required: true},
   title: {type: String},
   placement: {type: String as PropType<Placement>, default: 'top'},
   fallbackPlacement: {type: String, default: 'flip'},
-  triggers: {type: String, default: 'hover click'},
+  triggers: {type: String as PropType<Tooltip.Options["trigger"]>, default: 'hover click'},
   noFade: {type: Boolean, default: false},
   delay: {type: [Number, Object], default: 50},
   offset: {type: Number, default: 0},
@@ -32,6 +43,26 @@ const props = defineProps({
   customClass: {type: String},
   id: {type: String},
 })
+
+const showEvents = computed(() => {
+  return props.triggers
+    .split(" ")
+    .map(
+      (trigger) =>
+        showEventDictionary[trigger as keyof typeof showEventDictionary]
+    )
+    .filter(Boolean);
+});
+
+const hideEvents = computed(() => {
+  return props.triggers
+    .split(" ")
+    .map(
+      (trigger) =>
+        hideEventDictionary[trigger as keyof typeof showEventDictionary]
+    )
+    .filter(Boolean);
+});
 
 const popperInstance = ref()
 const tooltip = ref<HTMLElement>()
@@ -48,39 +79,39 @@ const classes = computed(() => [
 ])
 
 onMounted(() => {
-  target.value = document.getElementById(props.target)
+  const _target = document.getElementById(props.target);
+  if (_target) {
+    target.value = _target;
 
-  if (target.value) {
-    popperInstance.value = createPopper(target.value, tooltip.value as HTMLElement, {
-      placement: props.placement,
-      modifiers: [
-        {
-          name: 'offset',
-          options: {
-            offset: props.offset,
-          },
-        },
-      ],
-    })
+    popperInstance.value = createPopper(
+      target.value,
+      tooltip.value as HTMLElement,
+      {
+        placement: props.placement,
+      }
+    );
+
+    if (props.triggers !== "manual") {
+      showEvents.value.forEach((event) => {
+        useEventListener(target.value, event, () => {
+          tooltip.value?.classList.add("show");
+          popperInstance.value.update();
+        });
+      });
+
+      hideEvents.value.forEach((event) => {
+        useEventListener(target.value, event, () => {
+          tooltip.value?.classList.remove("show");
+        });
+      });
+
+      if (props.triggers.includes("click")) {
+        useEventListener(target.value, "click", () => {
+          tooltip.value?.classList.toggle("show");
+          popperInstance.value.update();
+        });
+      }
+    }
   }
-})
-
-function show() {
-  tooltip.value?.setAttribute('data-show', '')
-  popperInstance.value.update()
-  showTooltip.value = true
-}
-
-useEventListener(target, 'focus', show)
-useEventListener(target, 'mouseenter', show)
-
-useEventListener(target, 'blur', () => {
-  tooltip.value?.removeAttribute('data-show')
-  showTooltip.value = false
-})
-
-useEventListener(target, 'mouseleave', () => {
-  tooltip.value?.removeAttribute('data-show')
-  showTooltip.value = false
-})
+});
 </script>
